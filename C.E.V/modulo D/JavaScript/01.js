98560308c0f62e12f299cf73d52a0284bceea945
VitorVitima <yukkiegg@gmail.com>
2022-10-19 21:04:02 -0300

    if começo

---
+++ C.E.V/modulo D/JavaScript/01.js
@@ -0,0 +1,29 @@
+var velocidade = 40
+console.log (`O seu carro estava a ${velocidade}km/h`)
+if (velocidade > 60) {
+    console.log ('Você fou multado')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ C.E.V/modulo D/JavaScript/02.js
@@ -0,0 +1,9 @@
+var pais = 'Estados Unidos'
+
+if (pais != 'Brasil'){
+    console.log(`Você vive no ${pais}`)
+    console.log(`Você é estrangeiro`)
+} else {
+    console.log(`Você vive no ${pais}`)
+    console.log(`Você não é estrangeiro`)
+}
