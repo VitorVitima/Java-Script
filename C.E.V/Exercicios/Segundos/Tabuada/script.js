8a05dee8836ecc7f6fab4a44bf85b04784e5b067
VitorVitima <yukkiegg@gmail.com>
2022-10-27 21:57:35 -0300

    Excercício 2 finalizado

---
+++ C.E.V/Exercicios/Segundos/Tabuada/script.js
@@ -0,0 +1,17 @@
+
+function botao() {
+    var numberText = document.querySelector('#number').value
+    var inNumber = Number(numberText)
+    var mainFrame = document.querySelector('#tabela')
+    var resultado, multplicação
+    mainFrame.style.width = 'fit-content'
+    multplicação = 1
+    while (multplicação <= 10){
+        var span = document.createElement('span')
+        resultado = inNumber * multplicação
+        span.innerHTML = `${inNumber} X ${multplicação} = ${resultado}`
+        mainFrame.insertAdjacentElement("beforeend", span)
+        multplicação++
+    }
+
+}
